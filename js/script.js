8d748320d2a612667b3be4bada10aacb8c32b21a
cristian <cristian_enano_99@hotmail.com>
2020-11-16 23:38:43 -0600

    Correcion de responsive

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-(function () {
+(function() {
     "use strict"
-    
-    document.getElementById("btn-hamburguer").onclick = function(){
+
+    document.getElementById("btn-hamburguer").onclick = function() {
         let pages = document.querySelector(".paginas")
         let hasAppear = pages.style.visibility;
-        
-        if(screen.width > 800) pages.style.visibility = "visible";
-        else if(hasAppear == "visible") pages.style.visibility = "collapse";
+
+        if (screen.width > 800) pages.style.visibility = "visible";
+        else if (hasAppear == "visible") pages.style.visibility = "collapse";
         else pages.style.visibility = "visible";
-        
+
         console.log(hasAppear);
     }
 })();

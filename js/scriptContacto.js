07d5e1b7567b71a33ac7b350f38acb0aa2000a26
Jeffer48 <herrerajeffer@gmail.com>
2020-11-19 07:44:42 -0600

    arreglo titulos 4

--- js/scriptContacto.js
+++ js/scriptContacto.js
@@ -1,24 +1,21 @@
 (function() {
     
     (function titleSize(){
-        let windowWidth = window.innerWidth;
-        let windowHeight = window.innerHeight;
-        let minorWidth;
+        let windowWidth = window.outerWidth;
+        let windowHeight = window.outerHeight;
         let titulo = document.getElementById("titulo");
         let fontSize = window.getComputedStyle(titulo).getPropertyValue('font-size');
         
-        if(windowWidth < windowWidth) minorWidth = windowWidth;
-        else minorWidth = windowHeight;
-        if(minorWidth < 1000){
+        console.log("Ancho de ventana: "+windowWidth);
+        console.log("Alto de ventana: "+windowHeight);
+        
+        if(windowHeight < 1000){
             let newSize = windowWidth / 10;
             newSize += "px";
             console.log("Tamaño de letra: "+fontSize);
             console.log(newSize);
             titulo.style.fontSize = newSize;
         }
-        
-        console.log("Ancho de ventana: "+windowWidth);
-        console.log("Tamaño de letra: "+fontSize);
     })();
     
 })();

65e1f033d1ec50c820b0e6aa36d661b86509ceb9
Jeffer48 <herrerajeffer@gmail.com>
2020-11-19 22:14:01 -0600

    Contacto titulos

--- js/scriptContacto.js
+++ js/scriptContacto.js
@@ -1,19 +1,22 @@
 (function() {
     
     (function titleSize(){
-        let windowWidth = window.outerWidth;
-        let windowHeight = window.outerHeight;
+        let windowWidthO = window.outerWidth;
+        let windowWidthI = window.innerWidth;
         let titulo = document.getElementById("titulo");
         let fontSize = window.getComputedStyle(titulo).getPropertyValue('font-size');
         
-        console.log("Ancho de ventana: "+windowWidth);
-        console.log("Alto de ventana: "+windowHeight);
+        console.log(windowWidthO);
+        console.log(windowWidthI);
         
-        if(windowHeight < 1000){
-            let newSize = windowWidth / 10;
-            newSize += "px";
-            console.log("Tamaño de letra: "+fontSize);
-            console.log(newSize);
+        if(windowWidthO < 1000){
+            let newSize = windowWidthO / 2;
+            newSize += "%";
+            titulo.style.fontSize = newSize;
+        }
+        else if(windowWidthI < 1000){
+            let newSize = windowWidthI / 2;
+            newSize += "%";
             titulo.style.fontSize = newSize;
         }
     })();

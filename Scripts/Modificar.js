b2d50db6b8e0bbdff939bc92c7b94d3f96490645
cristian <cristian_enano_99@hotmail.com>
2020-11-24 14:41:19 -0600

    Agregamos las paginas del Administrador

--- Scripts/Modificar.js
+++ Scripts/Modificar.js
@@ -1,5 +1,21 @@
-(function() {
-    "use strict";
+/*"use strict";*/
+const cartas = document.querySelectorAll(".card");
+var paginaseleccionada = "primeros datos";
+
+function redirigir() {
+    paginaseleccionada = this;
+    /*paginaseleccionada = this;*/
+    /*alert(paginaseleccionada.dataset.carta);*/
+    window.location.replace("Seleccion.html");
+    /* <iframe src="index.html" frameborder="0"></iframe>*/
+    let paginaDicha = paginaseleccionada.dataset.carta;
+    sessionStorage.setItem("paginaSeleccionada", paginaDicha);
+}
+/*
+console.log(paginaseleccionada);
+export { redirigir, objeto }*/
 
 
-})();
+
+
+cartas.forEach(card => card.addEventListener("click", redirigir));

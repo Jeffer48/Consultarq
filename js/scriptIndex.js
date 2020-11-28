f927fa7bca2b7e2a9404c28bf3327e4857a697bd
Jeffer48 <herrerajeffer@gmail.com>
2020-11-27 21:09:14 -0600

    Arreglo menor

--- js/scriptIndex.js
+++ js/scriptIndex.js
@@ -1,10 +1,10 @@
-(function(){
-    window.onresize = function(){
-        if(window.outerWidth < 600){
+(function () {
+    (function () {
+        if (window.outerWidth < 600) {
             let tamaño = window.outerWidth + "px " + "100%;";
             let cadena = "background-size: ";
             cadena += tamaño;
             document.body.style = cadena;
         }
-    }
+    })();
 })();

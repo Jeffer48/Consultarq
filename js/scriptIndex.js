964a261b63588b85b925693ad88c507b1680d5c0
Jeffer48 <herrerajeffer@gmail.com>
2020-11-30 19:03:57 -0600

    Responsividad 2

--- js/scriptIndex.js
+++ js/scriptIndex.js
@@ -1,4 +1,4 @@
-window.onresize = function(){
+window.onresize = function () {
     console.log(window.outerWidth);
     if (window.outerWidth < 800) {
         let tamaño = window.outerWidth + "px " + "100%;";
@@ -6,4 +6,11 @@ window.onresize = function(){
         cadena += tamaño;
         document.body.style = cadena;
     }
+}
+
+window.screen.orientation.onchange = function () {
+    let tamaño = window.outerWidth + "px " + "100%;";
+    let cadena = "background-size: ";
+    cadena += tamaño;
+    document.body.style = cadena;
 }

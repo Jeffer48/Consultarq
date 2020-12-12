75b0e23b390c0a2c7a7a6b6edf96c93a08618e33
cristian <cristian_enano_99@hotmail.com>
2020-12-12 14:09:31 -0600

    Cambios en el login

--- Scripts/Modificar.js
+++ Scripts/Modificar.js
@@ -6,7 +6,7 @@ function redirigir() {
     paginaseleccionada = this;
     /*paginaseleccionada = this;*/
     /*alert(paginaseleccionada.dataset.carta);*/
-    window.location.replace("Seleccion.html");
+    window.location.replace("Modificar.html");
     /* <iframe src="index.html" frameborder="0"></iframe>*/
     let paginaDicha = paginaseleccionada.dataset.carta;
     sessionStorage.setItem("paginaSeleccionada", paginaDicha);
@@ -15,7 +15,9 @@ function redirigir() {
 console.log(paginaseleccionada);
 export { redirigir, objeto }*/
 
+<?php
 
+?>
 
 
 cartas.forEach(card => card.addEventListener("click", redirigir));
